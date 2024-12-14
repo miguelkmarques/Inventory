44e231345135b3e5c2592cd6184c9e0ba0cf8eb6
Miguel Marques <miguelkmarques@outlook.com>
2024-12-14 18:55:43 -0400

    feat: implement AddStockCommandHandler

--- InventoryVenturus/Features/Stock/Commands/Add/AddStockCommandHandler.cs
+++ InventoryVenturus/Features/Stock/Commands/Add/AddStockCommandHandler.cs
@@ -1,13 +1,37 @@
-﻿using InventoryVenturus.Repositories.Interfaces;
+﻿using InventoryVenturus.Features.Products.Notifications;
+using InventoryVenturus.Features.Stock.Notifications;
+using InventoryVenturus.Repositories;
+using InventoryVenturus.Repositories.Interfaces;
 using MediatR;
+using System.Transactions;
 
 namespace InventoryVenturus.Features.Stock.Commands.Add
 {
     public class AddStockCommandHandler(IStockRepository stockRepository, IMediator mediator) : IRequestHandler<AddStockCommand, bool>
     {
-        public Task<bool> Handle(AddStockCommand request, CancellationToken cancellationToken)
+        public async Task<bool> Handle(AddStockCommand command, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            using var transactionScope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+            try
+            {
+                var existingStock = await stockRepository.GetStockByProductIdAsync(command.ProductId);
+                if (existingStock is null)
+                {
+                    return false;
+                }
+                existingStock.Quantity += command.Quantity;
+                var result = await stockRepository.UpdateStockAsync(existingStock);
+                await mediator.Publish(new StockAddedNotification(existingStock.ProductId, command.Quantity, existingStock.Quantity, command.Price), cancellationToken);
+                if (result)
+                {
+                    transactionScope.Complete();
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
