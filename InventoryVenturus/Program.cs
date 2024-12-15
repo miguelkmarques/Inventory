6ecea01b982c7c67fdead3c09350cf728493f291
Miguel Marques <miguelkmarques@outlook.com>
2024-12-15 11:45:06 -0400

    feat: exclude program from code coverage

--- InventoryVenturus/Program.cs
+++ InventoryVenturus/Program.cs
@@ -6,75 +6,84 @@ using InventoryVenturus.Exceptions;
 using InventoryVenturus.Repositories;
 using InventoryVenturus.Repositories.Interfaces;
 using Serilog;
+using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
 
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .CreateLogger();
-
-try
+namespace InventoryVenturus
 {
-    Log.Information("starting server.");
-
-    var builder = WebApplication.CreateBuilder(args);
-
-    // Add serilog
-    builder.Host.UseSerilog((context, loggerConfiguration) =>
+    [ExcludeFromCodeCoverage]
+    public class Program
     {
-        loggerConfiguration.WriteTo.Console();
-        loggerConfiguration.ReadFrom.Configuration(context.Configuration);
-    });
+        public static async Task Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
 
-    // Add services to the container.
-    builder.Services.AddControllers();
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-    builder.Services.AddSingleton<IDataContext, DataContext>();
-    builder.Services.AddScoped<IProductRepository, ProductRepository>();
-    builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
-    builder.Services.AddScoped<IStockRepository, StockRepository>();
-    builder.Services.AddScoped<IErrorLogRepository, ErrorLogRepository>();
-    builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-    builder.Services.AddMediatR(cfg =>
-    {
-        cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
-        cfg.AddOpenBehavior(typeof(ExceptionLoggingBehavior<,>));
-        cfg.AddOpenBehavior(typeof(RequestResponseLoggingBehavior<,>));
-        cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
-    });
-    builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-    builder.Services.AddProblemDetails();
+            try
+            {
+                Log.Information("starting server.");
 
-    var app = builder.Build();
-    app.UseSerilogRequestLogging();
-    app.UseExceptionHandler();
+                var builder = WebApplication.CreateBuilder(args);
 
-    // Initialize database
-    var dataContext = app.Services.GetRequiredService<IDataContext>();
-    await dataContext.InitDatabase();
+                // Add serilog
+                builder.Host.UseSerilog((context, loggerConfiguration) =>
+                {
+                    loggerConfiguration.WriteTo.Console();
+                    loggerConfiguration.ReadFrom.Configuration(context.Configuration);
+                });
 
-    // Configure the HTTP request pipeline.
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
+                // Add services to the container.
+                builder.Services.AddControllers();
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+                builder.Services.AddSingleton<IDataContext, DataContext>();
+                builder.Services.AddScoped<IProductRepository, ProductRepository>();
+                builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+                builder.Services.AddScoped<IStockRepository, StockRepository>();
+                builder.Services.AddScoped<IErrorLogRepository, ErrorLogRepository>();
+                builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+                builder.Services.AddMediatR(cfg =>
+                {
+                    cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                    cfg.AddOpenBehavior(typeof(ExceptionLoggingBehavior<,>));
+                    cfg.AddOpenBehavior(typeof(RequestResponseLoggingBehavior<,>));
+                    cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                });
+                builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+                builder.Services.AddProblemDetails();
+
+                var app = builder.Build();
+                app.UseSerilogRequestLogging();
+                app.UseExceptionHandler();
+
+                // Initialize database
+                var dataContext = app.Services.GetRequiredService<IDataContext>();
+                await dataContext.InitDatabase();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+                app.MapControllers();
+
+                app.Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "server terminated unexpectedly");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
     }
-
-    app.UseHttpsRedirection();
-
-    app.UseAuthorization();
-
-    app.MapControllers();
-
-    app.Run();
 }
-catch (Exception ex)
-{
-    Log.Fatal(ex, "server terminated unexpectedly");
-}
-finally
-{
-    Log.CloseAndFlush();
-}
-
-
