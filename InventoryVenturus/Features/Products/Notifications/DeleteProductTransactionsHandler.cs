9611b473765b5f7950662cdb8f674b2213d24eea
Miguel Marques <miguelkmarques@outlook.com>
2024-12-15 11:37:46 -0400

    feat: implement DeleteProductTransactionsHandler

--- InventoryVenturus/Features/Products/Notifications/DeleteProductTransactionsHandler.cs
+++ InventoryVenturus/Features/Products/Notifications/DeleteProductTransactionsHandler.cs
@@ -1,13 +1,30 @@
-﻿using InventoryVenturus.Repositories.Interfaces;
+﻿using InventoryVenturus.Repositories;
+using InventoryVenturus.Repositories.Interfaces;
 using MediatR;
 
 namespace InventoryVenturus.Features.Products.Notifications
 {
     public class DeleteProductTransactionsHandler(ILogger<DeleteProductTransactionsHandler> logger, ITransactionRepository transactionRepository) : INotificationHandler<ProductDeletionRequestedNotification>
     {
-        public Task Handle(ProductDeletionRequestedNotification notification, CancellationToken cancellationToken)
+        public async Task Handle(ProductDeletionRequestedNotification notification, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var deletedCount = await transactionRepository.DeleteProductTransactionsAsync(notification.Id);
+                if (deletedCount > 0)
+                {
+                    logger.LogInformation("{deletedCount} Transactions deleted for product with ID: {Id}", deletedCount, notification.Id);
+                }
+                else
+                {
+                    logger.LogWarning("No transactions found for product with ID: {Id}", notification.Id);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error deleting transactions for product with ID: {Id}", notification.Id);
+                throw;
+            }
         }
     }
 }
